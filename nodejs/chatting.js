fc757d9a94e02c0d6bed46fddf81b96dd2d3ce19
koalakid1 <koalakid154@gmail.com>
2020-06-02 17:46:41 +0900

    nodejs4 / android1

---
+++ nodejs/chatting.js
@@ -0,0 +1,22 @@
+// ����� ������ �����մϴ�.
+var http = require('http');
+var fs = require('fs');
+var socketio = require('socket.io');
+
+// �� ������ ����ϴ�.
+var server = http.createServer(function (request, response) {
+  // HTMLPage.html ������ �н��ϴ�.
+  fs.readFile('HTMLPage.html', function (error, data) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end(data);
+  });
+}).listen(52273, function () {
+  console.log('Server Running at http://127.0.0.1:52273');
+});
+
+var io = socketio.listen(server);
+io.sockets.on('connection', function (socket) {
+  socket.on('message', function (data) {
+    io.sockets.emit('message', data);
+  });
+});
